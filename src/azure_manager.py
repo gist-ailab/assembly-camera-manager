a08533b89c673087a4b00dc65bc0c42596d02e5b
seungback <shback@gist.ac.kr>
2020-10-13 16:49:52 +0900

    feat: median filter on extrinsic calibration

--- src/azure_manager.py
+++ src/azure_manager.py
@@ -22,55 +22,91 @@ class AzureManager:
         calib_srv = rospy.Service('/{}/extrinsic_calibration'.format(self.camera_name), ExtrinsicCalibrate, self.calibrate_azure)
         rospy.loginfo("Starting azure_manager.py for {}".format(self.camera_name))
         self.cam_to_fids = []
+        self.filter_size = 10
 
     def calibrate_azure(self, msg):
 
-        self.is_sucess = False
+        self.is_sucess = True
         self.is_finish = False
         rospy.loginfo("Calibrating {}".format(self.camera_name))
         target_fid_id = msg.target_fiducial_ids
+
+        # get transform (fiducial -> map) N times
+        self.cam_to_fids = []
+        self.trans_dict = {}
+        self.quat_dict = {}
+        self.img_err_dict = {}
+        self.obj_err_dict = {}
+        self.n_sucess_dict = {}
+        self.n_iter = 0
         self.aruco_sub = rospy.Subscriber('/{}/fiducial_transforms'.format(self.camera_name), \
-                            FiducialTransformArray, self.fiducial_to_map, target_fid_id)
-        while not self.is_finish:
-            pass
+                                FiducialTransformArray, self.get_fiducial_to_map, target_fid_id)
+        while self.n_iter != self.filter_size:
+            rospy.sleep(0.1)
+
+        for fiducial_id in self.n_sucess_dict.keys():
+            rospy.loginfo("fid id: {} ==> n_sucess: {}/{}".format(fiducial_id, self.n_sucess_dict[fiducial_id], self.filter_size))
+            errors = self.img_err_dict[fiducial_id]
+            rospy.loginfo("img err =>  median: {:.3f} \t std: {:.3f}".format(np.median(errors), np.std(errors)))
+            errors = self.obj_err_dict[fiducial_id]
+            rospy.loginfo("obj err =>  median: {:.3f} \t std: {:.3f}".format(np.median(errors), np.std(errors)))
+
+            if self.n_sucess_dict[fiducial_id] != self.filter_size:
+                self.is_sucess = False
+        
+        # apply median filter
+        for fiducial_id in  self.trans_dict.keys():
+            trans_median = np.median(np.asarray(self.trans_dict[fiducial_id]), axis=0)
+            quat_median = np.median(np.asarray(self.quat_dict[fiducial_id]), axis=0)
+
+            static_tf = geometry_msgs.msg.TransformStamped()
+            static_tf.header.frame_id = "{}".format(self.header_frame_id)
+            static_tf.child_frame_id = "{}_camera_fid_{}".format(self.camera_name, fiducial_id)
+            
+            static_tf.transform.translation.x = trans_median[0]
+            static_tf.transform.translation.y = trans_median[1]
+            static_tf.transform.translation.z = trans_median[2]
+
+            static_tf.transform.rotation.x = quat_median[0]
+            static_tf.transform.rotation.y = quat_median[1]
+            static_tf.transform.rotation.z = quat_median[2]
+            static_tf.transform.rotation.w = quat_median[3]
+
+            rospy.loginfo("published static tf: {} -> {}_camera_fid_{}".format(\
+               self.header_frame_id, self.camera_name, fiducial_id, ))   
+            self.cam_to_fids.append(static_tf)
+
         return self.is_sucess 
 
 
-    def fiducial_to_map(self, msg, target_fiducial_ids):
-        self.cam_to_fids = []
-        n_sucess = 0
+    def get_fiducial_to_map(self, msg, target_fiducial_ids):
+        self.n_target_fiducial_ids = len(target_fiducial_ids)
+        self.header_frame_id = msg.header.frame_id
+
         for i, Fidtransform in enumerate(msg.transforms): 
             # publish target fiducials as tf
-            if Fidtransform.fiducial_id in target_fiducial_ids:
+            target_fiducial_id = Fidtransform.fiducial_id
 
-                static_tf = geometry_msgs.msg.TransformStamped()
-                static_tf.header.frame_id = "{}".format(msg.header.frame_id)
-                static_tf.child_frame_id = "{}_camera_fid_{}".format(self.camera_name, Fidtransform.fiducial_id)
-
+            if target_fiducial_id in target_fiducial_ids:
                 trans = Fidtransform.transform.translation
-                static_tf.transform.translation.x = trans.x
-                static_tf.transform.translation.y = trans.y
-                static_tf.transform.translation.z = trans.z
-
                 rot = Fidtransform.transform.rotation
-                static_tf.transform.rotation.x = rot.x
-                static_tf.transform.rotation.y = rot.y
-                static_tf.transform.rotation.z = rot.z
-                static_tf.transform.rotation.w = rot.w
-                
-                # get cam_to_map
-                H_cam_to_map = np.eye(4)
-                H_cam_to_map[:3, 3] = tf_trans.translation_matrix((trans.x, trans.y, trans.z))[:3, 3]
-                H_cam_to_map[:3, :3] = tf_trans.quaternion_matrix((rot.x, rot.y, rot.z, rot.w))[:3, :3]
+                # initialize dict
+                if target_fiducial_id not in self.trans_dict:
+                    self.trans_dict[target_fiducial_id] = [[trans.x, trans.y, trans.z]]
+                    self.quat_dict[target_fiducial_id] = [[rot.x, rot.y, rot.z, rot.w]]
+                    self.n_sucess_dict[target_fiducial_id] = 1
+                    self.img_err_dict[target_fiducial_id] = [Fidtransform.image_error]
+                    self.obj_err_dict[target_fiducial_id] = [Fidtransform.object_error]
+                else:
+                    self.trans_dict[target_fiducial_id].append([trans.x, trans.y, trans.z])
+                    self.quat_dict[target_fiducial_id].append([rot.x, rot.y, rot.z, rot.w])
+                    self.n_sucess_dict[target_fiducial_id] += 1
+                    self.img_err_dict[target_fiducial_id].append(Fidtransform.image_error)
+                    self.obj_err_dict[target_fiducial_id].append(Fidtransform.object_error)
 
-                rospy.loginfo("published static tf: {} -> {}_camera_fid_{}".format(\
-                    msg.header.frame_id, self.camera_name, Fidtransform.fiducial_id, ))   
-                self.cam_to_fids.append(static_tf)
-                n_sucess += 1
-
-        if n_sucess == len(target_fiducial_ids): self.is_sucess = True
-        self.is_finish = True
-        self.aruco_sub.unregister()
+        self.n_iter += 1
+        if self.n_iter == self.filter_size:
+            self.aruco_sub.unregister()
 
     def publish_map(self):
         if len(self.cam_to_fids) != 0:
